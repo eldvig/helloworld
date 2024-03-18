75db7dd0a095e439df8f336f974ee3ffcda30778
eldvig <eldvig@gmail.com>
2024-03-18 11:25:42 +0300

    Добавил вывод на 3х языках + отметил чем отличаются println от printf

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome! TEST_TEXT");
+        System.out.println("Привет Мир!");
+        System.out.println("Hello World!");
+        System.out.println("Guten Tag!");
+        System.out.printf("printf - не добавляет после себя перенос на новую строку");
+        System.out.println(", println - добавляет после себя перенос строки");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
